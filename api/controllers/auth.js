6b6bf64120b853d13a0760f66005007681989590
narendravs <narendravs_mca@yahoo.com>
2024-11-12 17:25:37 +0530

    Update auth.js

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -18,23 +18,7 @@ export const register = async (req, res, next) => {
 export const login = async (req, res, next) => {
   try {
    
-    console.log(req.body.username);
-    const user = await User.findOne({ username: req.body.username });
-    if (!user) return next(createError(404, "User not found"));
-    const isPasswordCorrect = bcrypt.compare(req.body.password, user.password);
-    if (!isPasswordCorrect)
-      return next(createError(400, "Wrong password or username"));
-    const token = jwt.sign(
-      { id: user._id, isAdmin: user.isAdmin },
-      process.env.JWT
-    );
-    const { password, isAdmin, ...otherDetails } = user._doc;
-    res
-      .cookie("access_token", token, {
-        httpOnly: true,
-      })
-      .status(200)
-      .json("hello");
+   res.status(200).json("hello");
   } catch (error) {
     next(error);
   }
