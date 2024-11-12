684bb03c955e817946ae0725cbe689fd034ee259
narendravs <narendravs_mca@yahoo.com>
2024-11-12 16:24:49 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import List from "./pages/list/List";
 import Home from "./pages/home/Home";
 import Hotel from "./pages/hotel/Hotel";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/hotels" element={<List />} />
         <Route path="/hotels/:id" element={<Hotel />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
