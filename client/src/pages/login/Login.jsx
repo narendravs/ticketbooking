a46e884dcbc1f195006f031f0fbae347ac74c34e
narendravs <narendravs_mca@yahoo.com>
2024-11-12 12:51:04 +0530

    Update Login.jsx

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -19,7 +19,7 @@ function Login() {
     dispatch({ type: "LOGIN_START" });
 
     try {
-      const res = await axios.post("auth/login", credentials);
+      const res = await axios.post("https://mern-ticketbooking-api.vercel.app/api/auth/login", credentials);
       console.log(res.data);
       dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
       navigate("/");
