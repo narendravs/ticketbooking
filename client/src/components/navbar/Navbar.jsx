572ab7dabf1ca4435dbd60f7ec846470f07fb4c9
narendravs <narendravs_mca@yahoo.com>
2024-11-12 15:32:54 +0530

    Revert "Update Navbar.jsx"
    This reverts commit 3836f2fa6048146cdb8cfb4e444c3afe26501b0a.

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,12 +1,10 @@
 import React, { useContext } from "react";
-import { Link,useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContex";
 import "./navbar.css";
 
 function Navbar() {
 const { user } = useContext(AuthContext);
-    const navigate = useNavigate();
-
 const handleLogin = () => {
     navigate("/login");
   };
