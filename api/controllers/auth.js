f7d9c14d6d1e407bb0fa76476589b1bd2101f947
narendravs <narendravs_mca@yahoo.com>
2024-11-12 17:43:52 +0530

    Update auth.js

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -24,10 +24,7 @@ export const login = async (req, res, next) => {
     const isPasswordCorrect = bcrypt.compare(req.body.password, user.password);
     if (!isPasswordCorrect)
       return next(createError(400, "Wrong password or username"));
-    const token = jwt.sign(
-      { id: user._id, isAdmin: user.isAdmin },
-      process.env.JWT
-    );
+    
     const { password, isAdmin, ...otherDetails } = user._doc;
     res
       .cookie("access_token", token, {
