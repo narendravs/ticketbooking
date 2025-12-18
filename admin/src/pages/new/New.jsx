c62ea82323d42ee83043b5e8b13eafc8d9ec49b1
narendravs <narendravs_mca@yahoo.com>
2025-12-18 12:39:07 +0530

    desing change and code review

--- admin/src/pages/new/New.jsx
+++ admin/src/pages/new/New.jsx
@@ -33,53 +33,58 @@ function New({ inputs, title }) {
   };
   return (
     <div className="new">
-      <Sidebar />
-      <div className="newContainer">
+      <div>
         <Navbar />
-        <div className="top">
-          <h1>{title}</h1>
-        </div>
-        <div className="bottom">
-          <div className="left">
-            <img
-              src={
-                file
-                  ? URL.createObjectURL(file)
-                  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
-              }
-              alt=""
-            />
+      </div>
+
+      <div className="container">
+        <Sidebar />
+        <div className="newContainer">
+          <div className="top">
+            <h1>{title}</h1>
           </div>
-          <div className="right">
-            <form>
-              <div className="formInput">
-                <label htmlFor="file" className="label">
-                  Image: <DriveFolderUploadOutlinedIcon className="icon" />
-                </label>
-                <input
-                  type="file"
-                  id="file"
-                  onChange={(e) => {
-                    setFile(e.target.files[0]);
-                  }}
-                />
-              </div>
-              {inputs.map((input) => (
-                <div className="formInput" key={input.id}>
-                  <label className="label">{input.label}</label>
+          <div className="bottom">
+            <div className="left">
+              <img
+                src={
+                  file
+                    ? URL.createObjectURL(file)
+                    : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+                }
+                alt=""
+              />
+            </div>
+            <div className="right">
+              <form>
+                <div className="formInput">
+                  <label htmlFor="file" className="label">
+                    Image: <DriveFolderUploadOutlinedIcon className="icon" />
+                  </label>
                   <input
-                    type={input.type}
-                    id={input.id}
-                    placeholder={input.placeholder}
-                    onChange={handleChange}
-                    className="input1"
+                    type="file"
+                    id="file"
+                    onChange={(e) => {
+                      setFile(e.target.files[0]);
+                    }}
                   />
                 </div>
-              ))}
-              <button className="button" onClick={handleClick}>
-                Send
-              </button>
-            </form>
+                {inputs.map((input) => (
+                  <div className="formInput" key={input.id}>
+                    <label className="label">{input.label}</label>
+                    <input
+                      type={input.type}
+                      id={input.id}
+                      placeholder={input.placeholder}
+                      onChange={handleChange}
+                      className="input1"
+                    />
+                  </div>
+                ))}
+                <button className="button" onClick={handleClick}>
+                  Send
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
