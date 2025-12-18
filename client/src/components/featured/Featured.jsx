4e497848bfc0c6b60f0df55f574aac46e92d8bf2
narendravs <narendravs_mca@yahoo.com>
2025-12-18 11:17:37 +0530

    enhanced the code according to the functionality

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -3,7 +3,7 @@ import useFetch from "../../hooks/useFetch";
 import "./featured.css";
 
 function Featured() {
-  const { data, loading, error } = useFetch(
+  const { loading } = useFetch(
     "/hotels/countByCity/cities/?cities=bangalore,mysore,tirupati"
   );
 
@@ -14,11 +14,7 @@ function Featured() {
       ) : (
         <>
           <div className="featuredItem">
-            <img
-              src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
-              alt=""
-              className="featuredImg"
-            />
+            <img src="\bangalore.jpg" alt="" className="featuredImg" />
             <div className="featuredTitles">
               <h1>Bagalore</h1>
               <h2>bangalore 4 properties</h2>
