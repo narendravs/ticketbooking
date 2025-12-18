4770deba93ea8a7248fef472f452183ac6623eb6
narendravs <narendravs_mca@yahoo.com>
2025-12-18 12:36:15 +0530

    enhance design and code review

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -10,22 +10,29 @@ import "./home.css";
 function Home() {
   return (
     <div className="home">
-      <Sidebar />
-      <div className="homeContainer">
+      <div>
         <Navbar />
-        <div className="widgets">
-          <Widget type="user" />
-          <Widget type="order" />
-          <Widget type="earning" />
-          <Widget type="balance" />
+      </div>
+
+      <div className="homeContainer">
+        <div className="sidebar">
+          <Sidebar />
         </div>
-        <div className="charts">
-          <Featured />
-          <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
-        </div>
-        <div className="listContainer">
-          <div className="listTitle">Latest Transactions</div>
-          <Table />
+        <div>
+          <div className="widgets">
+            <Widget type="user" />
+            <Widget type="order" />
+            <Widget type="earning" />
+            <Widget type="balance" />
+          </div>
+          <div className="charts">
+            <Featured />
+            <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
+          </div>
+          <div className="listContainer">
+            <div className="listTitle">Latest Transactions</div>
+            <Table />
+          </div>
         </div>
       </div>
     </div>
