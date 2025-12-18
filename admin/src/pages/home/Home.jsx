09121604e3939a5a2ab3bdffb903023b673ee966
narendravs <narendravs_mca@yahoo.com>
2025-12-18 16:50:14 +0530

    reviewed code and clean

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-// import Navbar from "../../components/navbar/Navbar";
-// import Sidebar from "../../components/sidebar/Sidebar";
-// import Widget from "../../components/widget/Widget";
-// import Featured from "../../components/featured/Featured";
-// import Chart from "../../components/chart/Chart";
-// import Table from "../../components/table/Table";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Widget from "../../components/widget/Widget";
+import Featured from "../../components/featured/Featured";
+import Chart from "../../components/chart/Chart";
+import Table from "../../components/table/Table";
 import "./home.css";
 
 function Home() {
