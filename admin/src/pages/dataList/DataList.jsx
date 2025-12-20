0adbb57852bc71ac6e5fdf7eba7bcc801b59bde6
narendravs <narendravs_mca@yahoo.com>
2025-12-20 17:42:01 +0530

    added new files for table data

---
+++ admin/src/pages/dataList/DataList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Datatable from "../../components/datatable/Datatable";
+import "./datalist.css";
+
+function List() {
+  return (
+    <div className="mainContainer">
+      <Datatable />
+    </div>
+  );
+}
+
+export default List;
