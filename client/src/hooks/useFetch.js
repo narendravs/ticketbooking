a0d2d690e05b622874d4d46b5a0bff758201acde
narendravs <narendravs_mca@yahoo.com>
2026-04-08 10:22:39 +0530

    refactor: integrate environment variables for API routing
    Switched from hardcoded Render URL to dynamic REACT_APP_API_URL_LOCAL. Fixed reFetch logic to properly concatenate BASE_URL and await the async request.

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+
 const useFetch = (url) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -9,11 +10,10 @@ const useFetch = (url) => {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const BASE_URL = "https://ticketbooking-5eoj.onrender.com/api";
+        //const BASE_URL = process.env.REACT_APP_API_URL;
+        const BASE_URL = process.env.REACT_APP_API_URL_LOCAL;
         const fullUrl = `${BASE_URL}${url}`;
-        console.log(fullUrl);
         const res = await axios.get(fullUrl);
-        console.log(res.data);
         setData(res.data);
       } catch (error) {
         setErros(error);
@@ -26,7 +26,8 @@ const useFetch = (url) => {
   const reFetch = async () => {
     setLoading(true);
     try {
-      const res = axios.get(url);
+      const BASE_URL = process.env.REACT_APP_API_URL_LOCAL;
+      const res = await axios.get(`${BASE_URL}${url}`);
       setData(res.setData);
     } catch (error) {
       setErros(error);
