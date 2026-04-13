221095dfade48205320661540f9f02ce8a49fb57
narendravs <narendravs_mca@yahoo.com>
2026-04-13 13:13:56 +0530

    refactor(client): rename api folder to axios for better naming clarity
    - Moved axios configuration from 'src/api/' to 'src/axios/'.
    
    - Updated folder structure to align with project naming conventions.

--- client/src/axios/axios.js
+++ client/src/axios/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 // This pulls from your .env file
-const BASE_URL =
-  process.env.REACT_APP_API_URL_LOCAL || "http://localhost:8000/api";
+const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8000/api";
 
 const publicRequest = axios.create({
   baseURL: BASE_URL,
