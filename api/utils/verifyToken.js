607491aa75d7c2a56e858277d4585900a55c9cf3
narendravs <narendravs_mca@yahoo.com>
2026-04-22 12:01:29 +0530

    refactor: review code and cleanup redundant console logs

--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -3,16 +3,13 @@ import createError from "../utils/error.js";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
-  console.log("....token.....");
-  console.log(token);
+
   if (!token) return next(createError(401, "You are not authenticated"));
 
   jwt.verify(token, process.env.JWT, (err, user) => {
     if (err) return next(createError(403, "Token is not valid"));
     if (!user) return next(createError(404, "User not found"));
     req.user = user;
-    console.log("....user.....");
-    console.log(req.user);
     next();
   });
 };
