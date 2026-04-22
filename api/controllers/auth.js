ecfb7f92d46bfaa4ede34956c7511eb5c2dc6dd2
narendravs <narendravs_mca@yahoo.com>
2026-04-22 11:45:43 +0530

    feat(auth): Implemented cloudinary API's to handle image uploads during new user adding from Dashboard
    - Refactored registration controller to prepare userData object
    
    - Added check for req.file to map Cloudinary URL to userData.img
    
    - Improved data structure before saving new User instance

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -3,6 +3,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import createError from "../utils/error.js";
 
+
 export const register = async (req, res, next) => {
   try {
     // 1. Check if user already exists
@@ -21,8 +22,21 @@ export const register = async (req, res, next) => {
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.password, salt);
 
-    // 3. Create and Save User
-    const newUser = new User({ ...req.body, password: hash });
+    // 3. Prepare User Data
+    // We create a base object first
+    const userData = { 
+      ...req.body, 
+      password: hash 
+    };
+
+    // 4. Check for file and add to userData if it exists
+    if (req.file) {
+      // If multer-storage-cloudinary is used, the URL is in req.file.path
+      userData.img = req.file.path;
+    }
+
+    // 5. Create and Save
+    const newUser = new User(userData);
     await newUser.save();
 
     res.status(200).json("User has been created.");
